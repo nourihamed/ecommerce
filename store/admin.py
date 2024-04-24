73e52d877d3bc8b0580224022f4b6e8d8805efcf
nourihamed <nourihamed@gmail.com>
2024-04-24 17:26:54 +0330

    models add to admin

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,17 @@ from django.contrib import admin
 
 from .models import Product , Category
 
-admin.site.register(Category)
+#admin.site.register(Category)
 
-admin.site.register(Product)
+#admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug' : ('name' ,  )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title' ,)}
